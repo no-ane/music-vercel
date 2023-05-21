50f81dec5a1fe60f5b2c5d3ef5967ab60b704e19
no-ane <c.wzpan@gmail.com>
2023-05-21 21:05:22 +0800

    update

--- api/hls-proxy.js
+++ api/hls-proxy.js
@@ -6,16 +6,33 @@ export default async function handler(req, res) {
 
   request.get(url, (err, requestResult) => {
       if (err) {
-	      console.log('err', err);
-      	res.send(err);
+        console.log('err', err);
+        res.send(err);
       } else {
-	      res.send(requestResult.body);
+        // res.send(requestResult.body);
+        res.writeHead(200, {
+          'Content-Type': ret['content-type'],
+          'Content-Length': ret['content-length']
+        });
+        res.end(Buffer.from(requestResult.body));
       }
     })
     .on('response', function(response) {
       // 设置响应头
-      if (response.headers['content-type']) res.setHeader('Content-Type', response.headers['content-type']);
-      if (response.headers['content-length']) res.setHeader('Content-Length', response.headers['content-length']);
-      if (response.headers['content-disposition']) res.setHeader('Content-Disposition', response.headers['content-disposition']);
+      if (response.headers['content-type']) {
+      	ret['content-type'] = response.headers['content-type'];
+      	res.setHeader('Content-Type', response.headers['content-type']);
+      }
+
+      if (response.headers['content-length']) {
+      	ret['content-length'] = response.headers['content-length'];
+      	res.setHeader('Content-Length', response.headers['content-length']);
+      }
+
+      if (response.headers['content-disposition']) {
+      	ret['content-disposition'] = response.headers['content-disposition'];
+      	res.setHeader('Content-Disposition', response.headers['content-disposition']);
+      }
+
     })
 }
