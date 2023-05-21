9558c157805c7b47d83c649aee2122c832eb6371
no-ane <c.wzpan@gmail.com>
2023-05-21 20:12:17 +0800

    test

--- api/hls-proxy.js
+++ api/hls-proxy.js
@@ -1,14 +1,14 @@
 import request from 'request';
 
 export default function handler(req, res) {
-  // 截取 /pan/proxy/ 后面的内容
   const url = decodeURIComponent(req.url.slice(19));
-  request.get(url)
-    .on('response', function(response) {
-      // 设置响应头
-      res.set('Content-Type', response.headers['content-type']);
-      res.set('Content-Length', response.headers['content-length']);
-      res.set('Content-Disposition', response.headers['content-disposition']);
-    })
-    .pipe(res);
+  res.send(url);
+  // request.get(url)
+  //   .on('response', function(response) {
+  //     // 设置响应头
+  //     res.set('Content-Type', response.headers['content-type']);
+  //     res.set('Content-Length', response.headers['content-length']);
+  //     res.set('Content-Disposition', response.headers['content-disposition']);
+  //   })
+  //   .pipe(res);
 }
