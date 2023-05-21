80eaad0eaa38dd05c9d4240ab0ae2f0df3e23ebe
no-ane <c.wzpan@gmail.com>
2023-05-21 20:29:01 +0800

    test

--- api/hls-proxy.js
+++ api/hls-proxy.js
@@ -14,8 +14,8 @@ export default async function handler(req, res) {
     })
     .on('response', function(response) {
       // 设置响应头
-      res.setHeader('Content-Type', response.headers['content-type']);
-      res.setHeader('Content-Length', response.headers['content-length']);
-      res.setHeader('Content-Disposition', response.headers['content-disposition']);
+      if (response.headers['content-type']) res.setHeader('Content-Type', response.headers['content-type']);
+      if (response.headers['content-length']) res.setHeader('Content-Length', response.headers['content-length']);
+      if (response.headers['content-disposition']) res.setHeader('Content-Disposition', response.headers['content-disposition']);
     })
 }
