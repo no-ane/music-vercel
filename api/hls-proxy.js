9401b12b543f584575e59fbdef0730c460c8dbea
no-ane <c.wzpan@gmail.com>
2023-05-21 21:14:23 +0800

    update

--- api/hls-proxy.js
+++ api/hls-proxy.js
@@ -1,38 +1,46 @@
 import request from 'request';
 
 export default async function handler(req, res) {
-  const url = decodeURIComponent(req.url.slice(19));
-  const ret = {};
 
-  request.get(url, (err, requestResult) => {
-      if (err) {
-        console.log('err', err);
-        res.send(err);
-      } else {
-        // res.send(requestResult.body);
-        res.writeHead(200, {
-          'Content-Type': ret['content-type'],
-          'Content-Length': ret['content-length']
-        });
-        res.end(Buffer.from(requestResult.body));
-      }
-    })
-    .on('response', function(response) {
-      // 设置响应头
-      if (response.headers['content-type']) {
-      	ret['content-type'] = response.headers['content-type'];
-      	res.setHeader('Content-Type', response.headers['content-type']);
-      }
+  const imageData = Buffer.from('FF0000', 'hex'); // 红色 RGB 值为 FF0000
+  res.writeHead(200, {
+    'Content-Type': 'image/jpeg',
+    'Content-Length': imageData.length
+  });
+  res.end(imageData);
+  
+  // const url = decodeURIComponent(req.url.slice(19));
+  // const ret = {};
 
-      if (response.headers['content-length']) {
-      	ret['content-length'] = response.headers['content-length'];
-      	res.setHeader('Content-Length', response.headers['content-length']);
-      }
+  // request.get(url, (err, requestResult) => {
+  //     if (err) {
+  //       console.log('err', err);
+  //       res.send(err);
+  //     } else {
+  //       // res.send(requestResult.body);
+  //       res.writeHead(200, {
+  //         'Content-Type': ret['content-type'],
+  //         'Content-Length': ret['content-length']
+  //       });
+  //       res.end(Buffer.from(requestResult.body));
+  //     }
+  //   })
+  //   .on('response', function(response) {
+  //     // 设置响应头
+  //     if (response.headers['content-type']) {
+  //     	ret['content-type'] = response.headers['content-type'];
+  //     	res.setHeader('Content-Type', response.headers['content-type']);
+  //     }
 
-      if (response.headers['content-disposition']) {
-      	ret['content-disposition'] = response.headers['content-disposition'];
-      	res.setHeader('Content-Disposition', response.headers['content-disposition']);
-      }
+  //     if (response.headers['content-length']) {
+  //     	ret['content-length'] = response.headers['content-length'];
+  //     	res.setHeader('Content-Length', response.headers['content-length']);
+  //     }
 
-    })
+  //     if (response.headers['content-disposition']) {
+  //     	ret['content-disposition'] = response.headers['content-disposition'];
+  //     	res.setHeader('Content-Disposition', response.headers['content-disposition']);
+  //     }
+
+  //   })
 }
