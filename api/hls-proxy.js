6c866f640f9507449baf3cbc2fa9761b6ca5b747
no-ane <c.wzpan@gmail.com>
2023-05-21 21:31:48 +0800

    update

--- api/hls-proxy.js
+++ api/hls-proxy.js
@@ -12,24 +12,28 @@ export default async function handler(req, res) {
         res.send(err);
       } else {
         // res.send(requestResult.body);
-        res.send(Buffer.from(requestResult.body, 'binary'));
+        res.setHeader({
+        	'Content-Type': ret['content-type'],
+        	'Content-Length': ret['content-length'],
+        })
+        res.send(Buffer.from(requestResult.body));
       }
     })
     .on('response', function(response) {
       // 设置响应头
       if (response.headers['content-type']) {
       	ret['content-type'] = response.headers['content-type'];
-      	res.setHeader('Content-Type', response.headers['content-type']);
+      	// res.setHeader('Content-Type', response.headers['content-type']);
       }
 
       if (response.headers['content-length']) {
       	ret['content-length'] = response.headers['content-length'];
-      	res.setHeader('Content-Length', response.headers['content-length']);
+      	// res.setHeader('Content-Length', response.headers['content-length']);
       }
 
       if (response.headers['content-disposition']) {
       	ret['content-disposition'] = response.headers['content-disposition'];
-      	res.setHeader('Content-Disposition', response.headers['content-disposition']);
+      	// res.setHeader('Content-Disposition', response.headers['content-disposition']);
       }
 
     })
