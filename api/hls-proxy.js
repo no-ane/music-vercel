f27d5a962c037fd03237a6dd22094b8ccaa98626
no-ane <c.wzpan@gmail.com>
2023-05-21 21:38:27 +0800

    update

--- api/hls-proxy.js
+++ api/hls-proxy.js
@@ -6,39 +6,31 @@ export default async function handler(req, res) {
   const url = decodeURIComponent(req.url.slice(19));
   const ret = {};
 
-  const data = await new Promise((resolve) => {
-    request.get(url, (err, requestResult) => {
-        if (err) {
-          console.log('err', err);
-          res.send(err);
-        } else {
-          // res.send(requestResult.body);
-          // res.setHeader({
-          //   'Content-Type': ret['content-type'],
-          //   'Content-Length': ret['content-length'],
-          // })
-          resolve(Buffer.from(requestResult.body));
-        }
-      })
-      .on('response', function(response) {
-        // 设置响应头
-        if (response.headers['content-type']) {
-          ret['content-type'] = response.headers['content-type'];
-          // res.setHeader('Content-Type', response.headers['content-type']);
-        }
-        if (response.headers['content-length']) {
-          ret['content-length'] = response.headers['content-length'];
-          // res.setHeader('Content-Length', response.headers['content-length']);
-        }
-        if (response.headers['content-disposition']) {
-          ret['content-disposition'] = response.headers['content-disposition'];
-          // res.setHeader('Content-Disposition', response.headers['content-disposition']);
-        }
-      })
-  })
+  request.get(url, (err, requestResult) => {
+      if (err) {
+        console.log('err', err);
+        res.send(err);
+      } else {
+        // res.send(requestResult.body);
+        res.send(Buffer.from(requestResult.body, 'binary'));
+      }
+    })
+    .on('response', function(response) {
+      // 设置响应头
+      if (response.headers['content-type']) {
+      	ret['content-type'] = response.headers['content-type'];
+      	res.setHeader('Content-Type', response.headers['content-type']);
+      }
 
-  res.setHeader('Content-Type', ret['content-type']),
-  res.setHeader('Content-Length', ret['content-length']),
-  res.send(data);
+      if (response.headers['content-length']) {
+      	ret['content-length'] = response.headers['content-length'];
+      	res.setHeader('Content-Length', response.headers['content-length']);
+      }
 
+      if (response.headers['content-disposition']) {
+      	ret['content-disposition'] = response.headers['content-disposition'];
+      	res.setHeader('Content-Disposition', response.headers['content-disposition']);
+      }
+
+    })
 }
