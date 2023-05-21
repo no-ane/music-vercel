f9ec3e7645ea0c00a45bc24e52d88e009b600419
no-ane <c.wzpan@gmail.com>
2023-05-21 20:22:35 +0800

    test

--- api/hls-proxy.js
+++ api/hls-proxy.js
@@ -4,9 +4,9 @@ export default async function handler(req, res) {
   const url = decodeURIComponent(req.url.slice(19));
   const ret = {};
 
-  request.get('https://res.cloudinary.com/practicaldev/image/fetch/s--c63-lUU4--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/ppmwtvozrqoapx29p4nm.png', (err, res) => {
-      console.log(err);
+  request.get(url, (err, res) => {
       if (err) {
+	      console.log('err', err);
       	res.send(err);
       } else {
 	      res.send(res.body)
